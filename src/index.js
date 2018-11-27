9f105a558bd0526c87f87f6908e6f9a535a21180
sethfork <sethfork@users.noreply.github.com>
2018-11-27 18:31:08 -0500

    Fix component index

---
+++ src/index.js
@@ -0,0 +1,46 @@
+export { default as Actionbar } from "./components/actionbar/Actionbar";
+
+export { default as Breadcrumbs } from "./components/breadcrumbs/Breadcrumbs";
+export { default as Crumb } from "./components/breadcrumbs/Crumb";
+
+export { default as Button } from "./components/button/Button";
+export { default as LinkButton } from "./components/button/LinkButton";
+export { default as SubmitButton } from "./components/button/SubmitButton";
+
+export { default as Card } from "./components/card/Card";
+export { default as CardActions } from "./components/card/CardActions";
+export { default as CardContent } from "./components/card/CardContent";
+export { default as CardHeader } from "./components/card/CardHeader";
+export { default as CardSubtitle } from "./components/card/CardSubtitle";
+export { default as CardTitle } from "./components/card/CardTitle";
+
+export { default as Icons } from "./components/icons/Icons";
+export { default as BountyIcon } from "./components/icons/BountyIcon";
+export { default as CarotIcon } from "./components/icons/CarotIcon";
+export { default as CheckIcon } from "./components/icons/CheckIcon";
+export { default as CollaterallIcon } from "./components/icons/CollaterallIcon";
+export {
+  default as CommonInterestIcon
+} from "./components/icons/CommonInterestIcon";
+export { default as DisputeIcon } from "./components/icons/DisputeIcon";
+export { default as EmailIcon } from "./components/icons/EmailIcon";
+export { default as EthereumIcon } from "./components/icons/EthereumIcon";
+export { default as GithubIcon } from "./components/icons/GithubIcon";
+export { default as LinkedInIcon } from "./components/icons/LinkedInIcon";
+export { default as MetamaskIcon } from "./components/icons/MetamaskIcon";
+export { default as OverviewIcon } from "./components/icons/OverviewIcon";
+export { default as RedditIcon } from "./components/icons/BountyIcon";
+export { default as RepoIcon } from "./components/icons/RepoIcon";
+export { default as TelegramIcon } from "./components/icons/TelegramIcon";
+export { default as TokenIcon } from "./components/icons/TokenIcon";
+export { default as TwitterIcon } from "./components/icons/TwitterIcon";
+
+export { default as Input } from "./components/input/Input";
+
+export { default as InputRow } from "./components/input-row/InputRow";
+
+export { default as KeyValue } from "./components/keyvalue/KeyValue";
+
+export { default as Logo } from "./components/logo/Logo";
+
+export { default as Overlay } from "./components/overlay/Overlay";
